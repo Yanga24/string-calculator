2eff6524e2aa6b4da49268e0767493ac21a6715f
Yanga24 <60698180+Yanga24@users.noreply.github.com>
2020-04-19 12:46:28 +0200

    first commit

---
+++ src/string_calculator.js
@@ -0,0 +1,37 @@
+let stringcalculator = {
+  add: function (numbers) {
+    var sum = 0,
+      re,
+      negatives = [];
+    if (numbers) {
+      if (numbers.substr(0, 2) === "//") {
+        re = new RegExp(quote(numbers.substr(2, 1)));
+        numbers = numbers.substr(4);
+      } else {
+        re = /[,\n]/;
+      }
+      var array = numbers.split(re);
+      array.forEach(function (element, index, array) {
+        if (Number(element) < 0) {
+          negatives.push(Number(element));
+        }
+        sum = sum + Number(element);
+      });
+    }
+    if (negatives.length > 0) {
+      throw Error("negatives not allowed: " + negatives);
+    } else {
+      return sum;
+    }
+  },
+};
+
+var quote = function (str) {
+  return str.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+};
+
+module.exports = stringcalculator;
+
+console.log();
+console.log();
+console.log();
